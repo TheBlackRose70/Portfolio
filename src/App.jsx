e5de5253bdbc89e104761fbd54456fd008f534e1
Andre Dixon <dixonandreh@gmail.com>
2021-11-27 11:23:14 -0500

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
-import Contact from "./components/contact/Contact"
+import Contact from "./components/contact/Contact";
+import "./app.scss"
 
 function App() {
   return (
